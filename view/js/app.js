de4553ec9293d66a14cccb85bff1f070aee2e464
Anda <ax.fiolle@gmail.com>
2017-12-02 21:56:57 +0100

    Chapitre modules

--- view/js/app.js
+++ view/js/app.js
@@ -1,5 +1,5 @@
 $("document").ready(function() {
-  $("#toplink,.chaplink").click(function(){
+  $("#toplink,.chaplink,.btn").click(function(){
     $(".box").addClass("hidden");
     var thisHref = $(this).attr("href");
     $(thisHref).removeClass("hidden");
