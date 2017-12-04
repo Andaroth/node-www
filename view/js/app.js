611e3882cd1870d3277e7c84e3b0ef404e1c92a5
Anda <ax.fiolle@gmail.com>
2017-12-04 10:38:53 +0100

    exercice socket

--- view/js/app.js
+++ view/js/app.js
@@ -3,6 +3,7 @@ $("document").ready(function() {
     e.preventDefault();
     $(".box").addClass("hidden");
     var thisHref = $(this).attr("href");
+    console.log(thisHref);
     window.location.hash = $(this).attr("href");
     $(thisHref).removeClass("hidden");
     $(document).scrollTop(0);
